44aaa2b0fe3ad47e41e1e06e62bf068a2e4bb231
Arka Dash <dasharka05@gmail.com>
2025-01-23 15:17:18 +0530

    feat: add user authentication and management routes, models, and middleware

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import {User} from '../models/user.model';
+import {User} from '../models/userModel';
 
 class AuthController {
     async register(req: Request, res: Response) {
